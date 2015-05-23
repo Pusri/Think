988407481c0d7fc8647374f3f50a4c3a0f0e4ba0
Pusri <pusri.x@gmail.com>
2015-05-23 21:59:47 +0800

    电信资源共享平台

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'sp_',
+    //密钥
+    "AUTHCODE" => '5zGWUpts3ZL2Q1JIWC',
+    //cookies
+    "COOKIE_PREFIX" => 'sPycB2_',
+);
+?>
